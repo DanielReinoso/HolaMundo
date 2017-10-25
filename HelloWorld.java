36bea5ec9fdcb970c77f629d2082d0870d25f369
DanielReinoso <33032876+DanielReinoso@users.noreply.github.com>
2017-10-25 12:15:52 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
-public class HelloWorld{
+public class HelloWorld
+{
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Daniel");
+		System.out.println("Hello Kevin");
+		Chicken chicken;
+		chicken = new Chicken();
+		chicken.initialize("lucy","cafe",2);
+		chicken.showData();
 	}
 }
