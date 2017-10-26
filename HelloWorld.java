67624584c8f641957e643bf9b858e4a087f4864f
DanielReinoso <mateo_riva@hotmail.com>
2017-10-26 18:11:51 -0500

    Deber Gallinas

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,23 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Kevin");
+		System.out.println("Hello Daniel");
 		Chicken chicken;
+		Chicken chicken1;
+		Chicken chicken2;
+		Chicken chicken3;
 		chicken = new Chicken();
-		chicken.initialize("lucy","cafe",2);
+		chicken1 = new Chicken();
+		chicken2 = new Chicken();
+		chicken3 = new Chicken();
+		chicken.initialize("Lucy","cafe",2);
 		chicken.showData();
+		chicken1.initialize1("Claudia");
+		chicken1.showData1();
+		chicken2.initialize1("Juana");
+		chicken2.showData2();
+		chicken3.initialize1("Pepita");
+		chicken3.showData3();
+		
 	}
 }
